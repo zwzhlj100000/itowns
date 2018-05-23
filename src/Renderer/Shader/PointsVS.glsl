9851ff9b5ff085bf3039f840cd7fa63c40fb3e49
Mathieu Brédif <mathieu.bredif@ign.fr>
2018-05-23 12:30:04 +0200

    points material autosizing

--- src/Renderer/Shader/PointsVS.glsl
+++ src/Renderer/Shader/PointsVS.glsl
@@ -7,9 +7,7 @@ precision highp int;
 attribute vec3 position;
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
-
 uniform float size;
-uniform vec2 resolution;
 
 uniform bool pickingMode;
 uniform float opacity;
@@ -26,16 +24,12 @@ void main() {
         vColor = vec4(mix(color, overlayColor.rgb, overlayColor.a), opacity);
     }
 
-    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-    gl_Position = projectionMatrix * mvPosition;
+    gl_Position = projectionMatrix * (modelViewMatrix * vec4( position, 1.0 ));
 
     if (size > 0.) {
         gl_PointSize = size;
     } else {
-        // automatic sizing
-        float slope = tan(1.0 / 2.0);
-        float projFactor =  -0.5 * resolution.y / (slope * mvPosition.z);
-        gl_PointSize = max(3.0, min(10.0, 0.05 * projFactor));
+        gl_PointSize = -size / gl_Position.w;
     }
 
     #include <logdepthbuf_vertex>
